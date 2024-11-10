476eb67e0294233c570377f9865d48567378d326
akash70629 <akash70629@gmail.com>
2024-11-10 16:00:37 +0530

    quiz link added

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -5,13 +5,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const icon = container.querySelector(".icon");
     const answers = container.querySelector(".answers");
 
-    container.addEventListener("click", () => {
+    container.addEventListener("click", (event) => {
+      // Prevent the click event from bubbling up to the document
+      event.stopPropagation();
+
       if (icon.classList.contains("active")) {
         icon.classList.remove("active");
         answers.style.maxHeight = null;
       } else {
         icon.classList.add("active");
         answers.style.maxHeight = answers.scrollHeight + "px";
+      }
+    });
+  });
+
+  // Close all FAQs when clicking outside
+  document.addEventListener("click", () => {
+    faqContainers.forEach((container) => {
+      const icon = container.querySelector(".icon");
+      const answers = container.querySelector(".answers");
+
+      if (icon.classList.contains("active")) {
+        icon.classList.remove("active");
+        answers.style.maxHeight = null;
       }
     });
   });
